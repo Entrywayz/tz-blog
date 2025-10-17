418dd07fd2048457f63a1c560a5e1c58cedd820d
Entrywayz <injector2281337@gmail.com>
2025-10-17 18:37:02 +0300

    test3

--- app/database.py
+++ app/database.py
@@ -6,6 +6,7 @@ import os
 #test db
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
 
+#for commit
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
